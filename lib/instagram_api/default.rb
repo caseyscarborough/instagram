d67ce5d5eab871a8fc287bebb19c849fcb1d90a9
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-24 20:10:50 -0400

    Remove Hash monkey patching.

--- lib/instagram_api/default.rb
+++ lib/instagram_api/default.rb
@@ -12,11 +12,3 @@ module Instagram
 
   end
 end
-
-class ::Hash
-  def method_missing(name)
-    return self[name] if key? name
-    self.each { |k,v| return v if k.to_s.to_sym == name }
-    super.method_missing name
-  end
-end
