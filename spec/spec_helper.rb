b7540675c993109b02b8400f6bfdbe0f5f8463b0
Casey Scarborough <caseyscarborough@gmail.com>
2013-08-26 21:06:38 -0400

    Refactor spec_helper.rb and update cassettes. Replace CONFIG hash with ENV hash.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,28 +2,32 @@ require 'instagram_api'
 require 'webmock/rspec'
 require 'vcr'
 
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-  config.filter_run :focus
-  config.include WebMock::API
+RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+  c.run_all_when_everything_filtered = true
+  c.filter_run :focus
+  c.include WebMock::API
 
-  CONFIG = YAML.load(File.read(File.expand_path('../config.yml', __FILE__)))
+  if File.exist?(File.expand_path('../config.yml', __FILE__))
+    config = YAML.load(File.read(File.expand_path('../config.yml', __FILE__)))
+    config.each { |k,v| ENV[k] = v unless v.kind_of? Hash }
+  end
+
 end
 
 VCR.configure do |c|
   c.configure_rspec_metadata!
-  c.filter_sensitive_data("<test_client_id>") do
-    CONFIG['test_client_id']
+  c.filter_sensitive_data("<INSTAGRAM_API_TEST_CLIENT_ID>") do
+    ENV['INSTAGRAM_API_TEST_CLIENT_ID']
   end
-  c.filter_sensitive_data("<test_client_secret>") do
-    CONFIG['test_client_secret']
+  c.filter_sensitive_data("<INSTAGRAM_API_TEST_CLIENT_SECRET>") do
+    ENV['INSTAGRAM_API_TEST_CLIENT_SECRET']
   end
-  c.filter_sensitive_data("<test_callback_url>") do
-    CONFIG['test_callback_url']
+  c.filter_sensitive_data("<INSTAGRAM_API_TEST_CALLBACK_URL>") do
+    ENV['INSTAGRAM_API_TEST_CALLBACK_URL']
   end
-  c.filter_sensitive_data("<test_access_token>") do
-    CONFIG['test_access_token']
+  c.filter_sensitive_data("<INSTAGRAM_API_TEST_ACCESS_TOKEN>") do
+    ENV['INSTAGRAM_API_TEST_ACCESS_TOKEN']
   end
   c.cassette_library_dir = 'spec/cassettes'
   c.default_cassette_options = {
@@ -36,19 +40,19 @@ VCR.configure do |c|
 end
 
 def test_client_id
-  CONFIG['test_client_id']
+  ENV['INSTAGRAM_API_TEST_CLIENT_ID']
 end
 
 def test_client_secret
-  CONFIG['test_client_secret']
+  ENV['INSTAGRAM_API_TEST_CLIENT_SECRET']
 end
 
 def test_callback_url
-  CONFIG['test_callback_url']
+  ENV['INSTAGRAM_API_TEST_CALLBACK_URL']
 end
 
 def test_access_token
-  CONFIG['test_access_token']
+  ENV['INSTAGRAM_API_TEST_ACCESS_TOKEN']
 end
 
 def test_client
